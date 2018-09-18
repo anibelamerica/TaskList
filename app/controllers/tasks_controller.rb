94e379521dad5da0949fa1fb1bcdc71fb9a8386f
Anibel America <ameriabel@gmail.com>
2018-09-18 11:58:07 -0700

    create the task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,21 @@
 TASKS = [
   "Clean your room",
   "Do CS Fundamentals",
-  "Meal Prep"
+  "Meal Prep",
+  "Finish your homework"
 ]
 
 class TasksController < ApplicationController
 
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
   end
 end
