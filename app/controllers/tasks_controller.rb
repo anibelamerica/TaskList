5902bdf143cc148e1045532d4923e38c53af5a29
Anibel America <ameriabel@gmail.com>
2018-09-19 15:45:55 -0700

    adds create action, fixes link to on tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,15 +23,18 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  # def create
-  #   @task = Task.new(
-  #     action: params[:task][:action],
-  #     description: params[:task][:description],
-  #     completion_date: params[:task][:completion_date]
-  #   )
-  #
-  #   if @task.save
-  #     redirect_to
+  def create
+    @task = Task.new(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: Date.parse(params[:task][:completion_date])
+    )
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
 
   end
 
