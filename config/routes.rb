72dd35475257891b97d571df46feba3bc86c3aab
Anibel America <ameriabel@gmail.com>
2018-09-22 14:56:11 -0700

    removed comments and redundant code

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,5 @@ Rails.application.routes.draw do
   resources :tasks
 
   patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete'
-  # get '/tasks', to: 'tasks#index', as: 'tasks'
-  #
-  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  # post '/tasks', to: 'tasks#create'
-  #
-  # get '/tasks/:id', to: 'tasks#show', as: 'task'
-  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  # patch '/tasks/:id', to: 'tasks#update'
-  # delete '/tasks/:id', to: 'tasks#destroy'
-
-
 
 end
